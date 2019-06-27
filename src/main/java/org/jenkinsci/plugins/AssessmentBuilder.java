cdd0c78670fb8d98d89b3db48f924c137246e52b
cofitost <zxcv091922@gmail.com>
2019-06-28 03:38:37 +0800

    fix #17

--- src/main/java/org/jenkinsci/plugins/AssessmentBuilder.java
+++ src/main/java/org/jenkinsci/plugins/AssessmentBuilder.java
@@ -390,11 +390,6 @@ public class AssessmentBuilder extends Builder implements SimpleBuildStep {
       }
     }
 
-    // Delete jenkins_home/tests/oop-hw1/src/main
-    deleteFile(testFilePath + "/src/main", listener);
-    // Delete jenkins_home/tests/oop-hw1/src/web for WEB project
-    deleteFile(testFilePath + "/src/web", listener);
-
     // Delete jenkins_home/workspace/t1234567_oop-hw1/pom.xml
     deleteFile(workspaceDir + "/" + jobName + "/pom.xml", listener);
     // Delete jenkins_home/workspace/t1234567_oop-hw1/config
